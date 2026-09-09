8925fb8a6d85783879cfb976a4e0d6cfa8690cd5
berat-berber <contact@beratberber.com>
2026-09-09 20:49:22 +0300

    feat: add short url model

---
+++ services/writeService/src/Data/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using src.Models;
+
+namespace src.Data;
+
+public partial class AppDbContext : DbContext
+{
+
+    
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ShortUrl> ShortUrls { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShortUrl>(entity =>
+        {
+            entity.HasKey(e => e.ShortCode).HasName("short_urls_pkey");
+
+            entity.ToTable("short_urls");
+
+            entity.Property(e => e.ShortCode).HasColumnName("short_code");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("now()")
+                .HasColumnName("created_at");
+            entity.Property(e => e.ExpiresAt).HasColumnName("expires_at");
+            entity.Property(e => e.OriginalUrl).HasColumnName("original_url");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ services/writeService/src/Models/ShortUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace src.Models;
+
+public partial class ShortUrl
+{
+    public string ShortCode { get; set; } = null!;
+
+    public string OriginalUrl { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? ExpiresAt { get; set; }
+}
